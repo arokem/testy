b971cf4aee6e7312fdd84877440144a8895f4e2b
Ariel Rokem <arokem@gmail.com>
2020-01-24 11:46:37 -0800

    Adds a doctest

--- tools.py
+++ tools.py
@@ -2,6 +2,22 @@ import numpy as np
 
 
 def calculate_area(r):
+    """
+    Calculates area of a circle
+
+    Parameters
+    ----------
+    r : number
+
+    Returns
+    -------
+    area : the area of circle with radius r
+
+    Examples
+    --------
+    >>> calculate_area(1)
+    3.141592653589793
+    """
     assert r >= 0, "Radius can only be non-negative"
     area = np.pi * r **2
     return area
