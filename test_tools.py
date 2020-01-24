b079afdb5a0f89eec09018154998127cfc9935e6
Ariel Rokem <arokem@gmail.com>
2020-01-24 11:18:14 -0800

    Adds test functions

---
+++ test_tools.py
@@ -0,0 +1,15 @@
+import numpy as np
+from tools import calculate_area, calculate_circ
+
+
+def test_calculate_area_pi():
+    # calculate_area(-1)
+    assert calculate_area(1) == -np.pi, "For radius of 1, area should be pi"
+
+
+def test_calculate_area_zero():
+    assert calculate_area(0) == 0
+
+
+def test_calculate_area_exp10():
+    assert calculate_area(np.exp(10)) == 1524191413.6768537
